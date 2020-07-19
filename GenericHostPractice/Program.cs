c300f1ef675384fe83336cf2b0672ad9154f97c4
Ginpay Iwatobi <y-toyoda@live.jp>
2020-07-19 10:48:52 +0900

    ハロワ

---
+++ GenericHostPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericHostPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Work!");
+        }
+    }
+}
